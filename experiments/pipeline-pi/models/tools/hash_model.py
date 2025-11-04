fad96ad261c79f75f0e8620b53d092b58e04c0ef
Esmee <esmeekraan2@gmail.com>
2025-11-04 14:02:15 +0100

    Stopped tracking personal experiments

--- experiments/pipeline-pi/models/tools/hash_model.py
+++
@@ -1,8 +0,0 @@
-import hashlib, sys
-
-p = sys.argv[1]  # pad naar model.tflite
-h = hashlib.sha256()
-with open(p, "rb") as f:
-    for chunk in iter(lambda: f.read(8192), b""):
-        h.update(chunk)
-print(h.hexdigest())
